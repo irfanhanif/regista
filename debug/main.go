f95935acd9e3bae553bca3a835a157170668a5e1
irfanhanif <ih.irfanhanif@gmail.com>
2020-03-30 17:59:23 +0700

    Publisher

--- debug/main.go
+++ debug/main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-    "context"
+	"context"
 
-    "github.com/dchest/uniuri"
-    kafka "github.com/segmentio/kafka-go"
+	"github.com/dchest/uniuri"
+	kafka "github.com/segmentio/kafka-go"
 )
 
 func main() {
-    w := KafkaWriter()
-    for i := 0; i < 1000000; i++ {
-        w.WriteMessages(context.Background(), KafkaMessages()...)
-    }
-    w.Close()
+	w := KafkaWriter()
+	for i := 0; i < 100000; i++ {
+		w.WriteMessages(context.Background(), KafkaMessages()...)
+	}
+	w.Close()
 }
 
 func KafkaWriter() *kafka.Writer {
-    return kafka.NewWriter(kafka.WriterConfig{
-        Brokers         : []string{"localhost:9092"},
-        Topic           : "order",
-        Async           : true,
-        RequiredAcks    : 0,
-    })
+	return kafka.NewWriter(kafka.WriterConfig{
+		Brokers:      []string{"localhost:9092"},
+		Topic:        "order",
+		Async:        true,
+		RequiredAcks: 0,
+	})
 }
 
 func KafkaMessages() []kafka.Message {
-    return []kafka.Message{
-        kafka.Message{
-            Value: []byte(uniuri.NewLen(300)),
-        },
-    }
+	return []kafka.Message{
+		kafka.Message{
+			Value: []byte(uniuri.NewLen(300)),
+		},
+	}
 }
-
